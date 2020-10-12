5673042e5159834beadd6c917405504a8f629297
hochej <37776098+hochej@users.noreply.github.com>
2020-10-12 18:29:02 +0200

    updated pseudo_atom.rs
    Changed the deserialisation. The JSON format is not approbiate for the storage of the parameters, since it only supports a few data types. Therefore we use now the rusty object notation as a data format, since it supports the main rust data types and is easy to write.

--- src/pseudo_atom.rs
+++ src/pseudo_atom.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
-use serde_json::from_str;
+//use serde_json::from_str;
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+use ron::de::from_str;
 
 fn get_nan_vec() -> Vec<f64> {
     vec![f64::NAN]
@@ -16,6 +17,7 @@ fn get_inf_value() -> f64 {
     f64::INFINITY
 }
 
+///
 #[derive(Serialize, Deserialize)]
 struct PseudoAtom {
     z: u8,
@@ -76,6 +78,17 @@ struct SlaterKosterTable {
     index_to_symbol: HashMap<u8, String>,
 }
 
+/// RepulsivePotentialTable should be a struct with the following members
+///
+/// z1,z2: atomic numbers of atom pair
+/// d: Vec, distance between atoms
+/// vrep: repulsive potential on the grid d
+///
+/// smooth_decay controls whether vrep and its derivatives are set abruptly to
+/// 0 after the cutoff radius or whether a smoothing function is added.
+/// WARNING: the smooting function can change the nuclear repulsion energy between
+/// atoms that are far apart. Therefore you should check visually the tails of
+/// the repulsive potential and check that the additional energy is not negligible.
 #[derive(Serialize, Deserialize)]
 struct RepulsivePotentialTable {
     vrep: Vec<f64>,
@@ -86,25 +99,34 @@ struct RepulsivePotentialTable {
 
 #[test]
 fn test_free_pseudo_atom() {
-    let path: &Path = Path::new("./src/param/slaterkoster/free_pseudo_atom/h.json");
+    let path: &Path = Path::new("./src/param/slaterkoster/free_pseudo_atom/h.ron");
     let data: String = fs::read_to_string(path).expect("Unable to read file");
-    let pseudo_atom: PseudoAtom = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    let pseudo_atom: PseudoAtom = from_str(&data).expect("RON was not well-formatted");
     assert_eq! {pseudo_atom.z, 1};
 }
 
 #[test]
 fn test_confined_pseudo_atom() {
-    let path: &Path = Path::new("./src/param/slaterkoster/confined_pseudo_atom/h.json");
+    let path: &Path = Path::new("./src/param/slaterkoster/confined_pseudo_atom/h.ron");
     let data: String = fs::read_to_string(path).expect("Unable to read file");
-    let pseudo_atom: PseudoAtom = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    let pseudo_atom: PseudoAtom = from_str(&data).expect("RON was not well-formatted");
     assert_eq! {pseudo_atom.z, 1};
 }
 
 #[test]
 fn test_slako_tables() {
-    let path: &Path = Path::new("./src/param/slaterkoster/slako_tables/h_h.json");
+    let path: &Path = Path::new("./src/param/slaterkoster/slako_tables/h_h.ron");
     let data: String = fs::read_to_string(path).expect("Unable to read file");
-    let slako_table : SlaterKosterTable = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    let slako_table : SlaterKosterTable = from_str(&data).expect("RON was not well-formatted");
     assert_eq! {slako_table.z1, 1};
     assert_eq! {slako_table.z2, 1};
+}
+
+#[test]
+fn test_repulsive_potential_tables() {
+    let path: &Path = Path::new("./src/param/repulsive_potential/reppot_tables/h_h.ron");
+    let data: String = fs::read_to_string(path).expect("Unable to read file");
+    let reppot_table : RepulsivePotentialTable = from_str(&data).expect("RON was not well-formatted");
+    assert_eq! {reppot_table.z1, 1};
+    assert_eq! {reppot_table.z2, 1};
 }
