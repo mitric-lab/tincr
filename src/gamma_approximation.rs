40319d6430c1a8f3b397442dffabebffaf9a4ec6
hochej <37776098+hochej@users.noreply.github.com>
2020-10-17 13:19:37 +0200

    added switching functions
    the switching functions are implemented at the moment as enums. I am not sure if this is the best way to distinguish the different switching functions. Furthermore, we have to very similiar functions for the switching function value and the value of derivate. It might be better to use only kind of definition and distinguish if we want to compute the function value or the derivative.

--- src/gamma_approximation.rs
+++ src/gamma_approximation.rs
@@ -1,6 +1,7 @@
+use libm;
 use std::collections::HashMap;
-use std::f32::consts::PI;
 
+const PI_SQRT: f64 = 1.7724538509055159;
 
 /// The decay constants for the gaussian charge fluctuations
 /// are determined from the requirement d^2 E_atomic/d n^2 = U_H.
@@ -22,14 +23,13 @@ pub fn gaussian_decay(
     r_lr: f64,
     lc_flag: bool,
 ) -> HashMap<u8, f64> {
-
     let mut sigmas: HashMap<u8, f64> = HashMap::new();
 
     // ATTENTION LC_FLAG IS OVERWRITTEN HERE
     let lc_flag = true;
     if lc_flag == false {
         for (z, u) in hubbard_u.iter() {
-            sigmas.insert(*z, 1.0 / (*u * PI.sqrt()));
+            sigmas.insert(*z, 1.0 / (*u * PI_SQRT));
         }
     } else {
         // do something else
@@ -39,3 +39,48 @@ pub fn gaussian_decay(
     }
     return sigmas;
 }
+
+enum SwitchingFunction {
+    // f(R) = erf(R/Rlr)
+    ErrorFunction,
+    // f(R) = erf(R/Rlr) - 2/(sqrt(pi)*Rlr) * R * exp(-1/3 * (R/Rlr)**2)
+    ErrorFunctionGaussian,
+    // f(R) = 0
+    NoSwitching,
+}
+
+fn switching(s_func: SwitchingFunction, r: f64, r_lr: f64) -> f64 {
+    let result: f64 = match s_func {
+        SwitchingFunction::ErrorFunction => libm::erf(r / r_lr),
+        SwitchingFunction::ErrorFunctionGaussian => {
+            if r < 1.0e-8 {
+                0.0
+            } else {
+                libm::erf(r / lr) / r
+                    - 2.0 / (PI_SQRT * r_lr) * (-1.0 / 3.0 * (r / r_lr).pow(2)).exp()
+            }
+        }
+        SwitchingFunction::NoSwitching => 0.0,
+    };
+    return result;
+}
+
+fn deriv_switching(s_func: SwitchingFunction, r: f64, r_lr: f64) -> f64 {
+    let result: f64 = match s_func {
+        SwitchingFunction::ErrorFunction => 2.0 / (PI_SQRT * r_lr) * (-(r / r_lr).powi(2)).exp(),
+        SwitchingFunction::ErrorFunctionGaussian => {
+            if r < 1.0e-8 {
+                0.0
+            } else {
+                let r2: f64 = (r / r_lr).powi(2);
+                4.0 / (3.0 * PI_SQRT * r_lr.powi(3)) * (-r2 / 3.0).exp() * r
+                    + 2.0 / (PI_SQRT * r_lr) * -r2.exp() / r
+                    - libm::erf(r / r_lr) / r.powi(2)
+            }
+        }
+        SwitchingFunction::NoSwitching => 0.0,
+    };
+    return result;
+}
+
+//pub fn gamma_function (sigmas: HashMap<u8, f64>)
