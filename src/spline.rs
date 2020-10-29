d7f08b85b8d092c8c887b2cdc15a2f8ba17f55a2
hochej <37776098+hochej@users.noreply.github.com>
2020-10-29 14:54:02 +0100

    work on spline interpolation

--- src/spline.rs
+++ src/spline.rs
@@ -82,7 +82,7 @@ fn fp_curf(
     nest: usize,
     n: usize,
     t: &mut Vec<f64>,
-    c: Vec<f64>,
+    mut c: Vec<f64>,
     fp: f64,
     wrk: Vec<f64>,
     lwrk: usize,
@@ -90,8 +90,10 @@ fn fp_curf(
     ier: i8,
     nmax: usize,
     nrdata: &mut Vec<usize>,
-    fpint: Vec<f64>,
+    mut fpint: Vec<f64>,
 ) -> f64 {
+    let nmin: usize = 2 * k as usize;
+
     let tol: f64 = 1e-3;
     let acc: f64 = tol * s;
     let k1 = k as usize + 1;
@@ -107,7 +109,7 @@ fn fp_curf(
     let fpold: f64;
     let fp0: f64;
     let nplus: usize;
-    // if(s.gt.0.0d0) go to 45
+    // if iopt < 0 skip this and go to main loop
     if s > 0.0 {
         if iopt != 0 || n != nmin {
             fp0 = fpint[n - 1];
@@ -146,7 +148,7 @@ fn fp_curf(
     }
     // main loop for the different sets of knots. m is a save upper bound
     // for the number of trials.
-    for iter in 0..m - 1 {
+    for iter in 1..(m + 1) {
         if n == nmin {
             ier = -2
         };
@@ -155,53 +157,225 @@ fn fp_curf(
         // find the position of the additional knots which are needed for
         // the b-spline representation of s[x].
         let nk1: usize = n - k1;
-        let mut i: usize = n - 1;
-        for j in 0..k {
-            t[j] = xb;
-            t[i] = xe;
+        let mut i: usize = n;
+        for j in 1..(k1 + 1) {
+            t[j - 1] = xb;
+            t[i - 1] = xe;
             i = i - 1;
         }
         //  compute the b-spline coefficients of the least-squares spline
         //  sinf(x). the observation matrix a is built up row by row and
         //  reduced to upper triangular form by givens transformations.
         //  at the same time fp=f(p=inf) is computed.
-        let fp: f64 = 0.0;
+        let mut fp: f64 = 0.0;
         // initialize the observation matrix a
-        for i in 0..nk1 - 1 {
-            z[i] = 0.0;
+        for i in 1..(nk1 + 1) {
+            z[i - 1] = 0.0;
             // TODO: we dont need this
-            for j in 0..k {
-                a[[i, j]] = 0.0;
+            for j in 1..(k - 1) {
+                a[[i - 1, j - 1]] = 0.0;
             }
         }
         let mut l: usize = k as usize;
-        for it in 0..m - 1 {
+        for it in 1..(m + 1) {
             // fetch the current data point x[it], y[it]
-            let xi: f64 = x[it];
-            let wi: f64 = w[it];
-            let yi: f64 = y[it];
+            let xi: f64 = x[it - 1];
+            let wi: f64 = w[it - 1];
+            let yi: f64 = y[it - 1] * wi;
             // search for knot interval t[l] <= xi < t[l+1].
-            while xi >= t[l + 1] && l == nk1 - 1 {
+            while xi >= t[l + 1] && l != nk1 {
                 l = l + 1;
             }
             // evaluate the (k+1) non-zero b-splines at xi and store them in q
-            let mut h = fbspl(xi, &t, k, n, l, h);
-            for i in 0..k {
-                q[[it, i]] = h[i];
-                h[i] = h[i] * wi;
+            let mut h: Vec<f64> = fbspl(xi, &t, k, n, l, h);
+            for i in 1..(k1 + 1) {
+                q[[it - 1, i - 1]] = h[i - 1];
+                h[i - 1] = h[i - 1] * wi;
             }
             j = l - k1;
-            for i in 0..k1 {
+            for i in 1..(k1 + 1) {
                 j = j + 1;
-                piv = h[i];
+                piv = h[i - 1];
                 if piv != 0.0 {
                     // calculate the parameters of the givens transformation
                     // CALL FPGIVS(piv,A(j,1),cos,sin)
                     // transformations to right hand side.
                     // CALL FPROTA(cos,sin,yi,Z(j))
+                    if i != k1 {
+                        let mut i2: usize = 1;
+                        let mut i3: usize = i + 1;
+                        for i1 in i3..(k1 + 1) {
+                            i2 = i2 + 1;
+                            // transformations to left hand side
+                            // call fprota(cos,sin,h(i1),a(j,i2))
+                        }
+                    }
                 }
             }
-            // rotate the new row of the observation matrix into triangle
+            //  add contribution of this row to the sum of squares of residual
+            //  right hand sides.
+            fp = fp + yi * yi;
+        }
+        if ier == -2 {
+            fp0 = fp
+        }
+        fpint[n - 1] = fp0;
+        fpint[n - 2] = fpold;
+        nrdata[n - 1] = nplus;
+        //  backward substitution to obtain the b-spline coefficients.
+        // call fpback(a,z,nk1,k1,c,nest)
+        //  test whether the approximation sinf(x) is an acceptable solution .
+        assert!(
+            iopt >= 0,
+            "the approximation sinf[x] is not an acceptable solution"
+        );
+        let fpms: f64 = fp - s;
+        assert!(fpms.abs() >= acc);
+        // if f(p=inf) < s accept the choice of knots
+        if fpms >= 0.0 {
+            //  if n = nmax, sinf(x) is an interpolating spline.
+            // if(n.eq.nmax) go to 430
+            // increase the number of knots.
+            // if n=nest we cannot increase the number of knots because of
+            //  the storage capacity limitation.
+            if ier != 0 {
+                nplus = 1;
+                ier = 0;
+            } else {
+                npl1 = nplus * 2;
+                rn = nplus;
+                if fpold - fp > acc {
+                    npl1 = rn * fpms / (fpold - fp);
+                    //nplus = [nplus * 2, [npl1 as usize, nplus/2, 1 as usize].max()].min();
+                }
+            }
+            fpold = fp;
+            //  compute the sum((w(i)*(y(i)-s(x(i))))**2) for each knot interval
+            //  t(j+k) <= x(i) <= t(j+k+1) and store it in fpint(j),j=1,2,...nrint.
+            let mut fpart: f64 = 0.0;
+            i = 1;
+            l = k2;
+            let mut new: usize = 0;
+            for it in 1..(m + 1) {
+                if x[it - 1] >= t[l - 1] && l <= nk1 {
+                    new = 1;
+                    l = l + 1;
+                }
+                let mut term: f64 = 0.0;
+                l0 = l - k2;
+                for j in 1..(k1 + 1) {
+                    l0 = l0 + 1;
+                    term = term + c[l0 - 1] * q[[it - 1, j - 1]];
+                }
+                term = (w[it - 1] * (term - y[it - 1])).powi(2);
+                fpart = fpart + term;
+                if new != 0 {
+                    store = term * 0.50;
+                    fpint[i - 1] = fpart - store;
+                    i = i + 1;
+                    fpart = store;
+                    new = 0;
+                }
+                fpint[nrint - 1] = fpart;
+                for l in 1..(nplus + 1) {
+                    //  add a new knot.
+                    // call fpknot(x,m,t,n,fpint,nrdata,nrint,nest,1)
+                    //  if n=nmax we locate the knots as for interpolation.
+                    // if(n.eq.nmax) go to 10
+                    //  test whether we cannot further increase the number of knots.
+                    // if(n.eq.nest) go to 200
+                }
+            }
+        }
+    }
+
+    // PART 2
+    // call fpdisc
+    // inital value for p
+    let p1: f64 = 0.0;
+    let f1: f64 = fp0 - s;
+    let p3: f64 = -1.0;
+    let f3: f64 = fpms;
+    p = 0;
+    for i in 1..(nk1 + 1) {
+        p = p + a[[i - 1, 0]]
+    }
+    let rn = nk1;
+    p = rn / p;
+    let ich1: usize = 0;
+    let ich2: usize = 0;
+    let n8: usize = n - nmin;
+    // iteration process to find the root of f[p] = s
+    for iter in 1..(maxit + 1) {
+        //  the rows of matrix b with weight 1/p are rotated into the
+        //  triangularised observation matrix a which is stored in g.
+        let pinv = one / p;
+        for i in 1..(nk1 + 1) {
+            c[i - 1] = z[i - 1];
+            g[[i - 1, k2]] = 0.0;
+            for j in 1..(k1 + 1) {
+                g[[i - 1, j - 1]] = a[[i - 1, j - 1]];
+            }
+            for it in 1..(n8 + 1) {
+                for i in 1..(k2 + 1) {
+                    h[i - 1] = b[[it - 1, i - 1]] * pinv;
+                }
+                yi = 0.0;
+                for j in it..(nk1 + 1) {
+                    let piv = h[0];
+                    //  calculate the parameters of the givens transformation.
+                    //  call fpgivs(piv,g(j,1),cos,sin)
+                    //  transformations to right hand side.
+                    //  call fprota(cos,sin,yi,c(j))
+                    if j != nk1 {
+                        if j > n8 {
+                            i2 = nk1 - j;
+                        } else {
+                            i2 = k1;
+                        }
+                        for i in 1..(i2 + 1) {
+                            // transformations to the left hand side
+                            i1 = i + 1;
+                            // call fprota(cos,sin,h(i1),g(j,i1))
+                            h[i - 1] = h[i1 - 1];
+                        }
+                        h[i2] = 0.0;
+                    }
+                }
+            }
+            // backward substitution to obtain the b-spline coefficients
+            // call fpback(g,c,nk1,k2,c,nest)
+            // computation of f[p]
+            let mut fp: f64 = 0.0;
+            l = k2;
+            for it in 1..(m + 1) {
+                if x[it - 1] >= t[l - 1] && l <= nk1 {
+                    l = l + 1;
+                }
+                l0 = l - k2;
+                term = 0.0;
+                for j in 1..(k1 + 1) {
+                    l0 = l0 + 1;
+                    term = term + c[l0 - 1] * q[[it - 1, j - 1]];
+                }
+                fp = fp + (w[it - 1] * (term - y[it - 1])).powi(2);
+            }
+            // test whether the approximation sp(x) is an acceptable solution
+            fpms = fp - s;
+            // test whether the maximal number of iterations is reached
+
+            // carry out one more step of the iteration process
+
+            // our initial choice of p is too large
+
+            // our initial choice of p is too small
+
+            // test whether the iteration process proceeds as theoretically
+            // expected
+
+            // find the new value for p
+            let (p, p1, f1, p3, f3): (f64, f64, f64, f64, f64) = fprati(p1, f1, p2, f2, p3, f3);
+            // error codes and messages
         }
     }
     return 1.0;
@@ -222,7 +396,7 @@ fn fp_curf(
 ///         subset of data points y(j) such that
 ///             t[j] < y[j] < t[j+k+1], j=0,1,2,...,n-k-1
 ///
-///  The original subroutine in FITPACK by Paul Dierckx is named fpcheck
+///  The original subroutine in FITPACK by Paul Dierckx is named fpchec
 fn check_knots(x: &Vec<f64>, t: &Vec<f64>, k: u8, m: usize, n: usize) -> u8 {
     let k1: usize = k as usize + 1;
     let nk1: usize = n - k1;
@@ -403,4 +577,35 @@ fn fpdisc(t: Vec<f64>, k2: usize, n: usize, b: Array2<f64>) -> f64 {
         }
     }
     return 1.0;
+}
+
+/// given three points (p1, f1), (p2, f2) and (p3, f3), function fprati
+/// gives the value of p such that the rational interpolating function
+/// of the form r(p) = (u*p+v)/(p+w) equals zero at p.
+/// The original subroutine in FITPACK by Paul Dierckx is named fprati
+fn fprati(
+    mut p1: f64,
+    mut f1: f64,
+    p2: f64,
+    f2: f64,
+    mut p3: f64,
+    mut f3: f64,
+) -> (f64, f64, f64, f64, f64) {
+    let p: f64 = if p3 <= 0.0 {
+        (p1 * (f1 - f3) * f2 - p2 * (f2 - f3) * f1) / ((f1 - f2) * f3)
+    } else {
+        let h1: f64 = f1 * (f2 - f3);
+        let h2: f64 = f2 * (f3 - f1);
+        let h3: f64 = f3 * (f1 - f2);
+        -(p1 * p2 * h3 + p2 * p3 * h1 + p3 * p1 * h2) / (p1 * h1 + p2 * h2 + p3 * h3)
+    };
+    // adjust the value of p1,f1,p3 and f3 such that f1 > 0 and f3 < 0
+    if f2 >= 0.0 {
+        p1 = p2;
+        f1 = f2;
+    } else {
+        p3 = p2;
+        f3 = f2;
+    }
+    return (p, p1, f1, p3, f3);
 }
