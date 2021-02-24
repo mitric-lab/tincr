1897c7b25ca8a996d66a73cd3f9eb658b4f85844
richard <richard.einsele@gmail.com>
2021-02-24 18:31:49 +0100

    started on internal coordinates from geomeTRIC, problematic

--- src/internal_coordinates.rs
+++ src/internal_coordinates.rs
@@ -2,12 +2,20 @@ use crate::constants;
 use crate::defaults;
 use crate::gradients;
 use crate::Molecule;
+use itertools::Itertools;
 use ndarray::prelude::*;
 use ndarray::Data;
 use ndarray::{Array2, Array4, ArrayView1, ArrayView2, ArrayView3};
 use ndarray_einsum_beta::*;
 use ndarray_linalg::*;
 use peroxide::prelude::*;
+use petgraph::algo::*;
+use petgraph::data::*;
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::*;
+use petgraph::stable_graph::*;
+use crate::molecule::distance_matrix;
+use std::f64::consts::PI;
 
 pub fn cartesian_to_internal(coord_vector: &Array1<f64>, mol: &Molecule) {
     let masses: Array1<f64> = build_masses(mol);
@@ -69,3 +77,133 @@ pub fn wrap_angles() {
     //     Angles outside these ranges are wrapped back to the equivalent
     // angle inside the range.
 }
+
+pub fn build_primitive_internal_coords(mol:&Molecule){
+    for fragment in mol.sub_graphs.iter(){
+        let mut coordinate_matrix:Array2<f64> = Array::zeros((fragment.node_count(),3));
+        for (index,z_i) in fragment.node_indices().enumerate(){
+            coordinate_matrix.slice_mut(s![index,..]).assign(&mol.positions.slice(s![z_i.index(),..]));
+        }
+        let coordinate_vector:Array1<f64> = coordinate_matrix.into_shape(3*fragment.node_count()).unwrap();
+        // for primitive internal coords
+        // first distance
+        // then angles
+        // then dihedrals
+        let mut internal_coords:Vec<_> = Vec::new();
+
+        //distances
+        for edge_index in fragment.edge_indices(){
+            let (a,b) = fragment.edge_endpoints(edge_index).unwrap();
+            //internal_coords.push(mol.distance_matrix[[a.index(),b.index()]]);
+            let dist:Distance = Distance::new(a.index(),b.index(),&mol.distance_matrix);
+            internal_coords.push(dist);
+        }
+
+        //angles
+        let linthre:f64= 0.95;
+        for b in fragment.node_indices(){
+            for a in fragment.neighbors(b){
+                for c in fragment.neighbors(b){
+                    if a.index() < c.index(){
+                        let angl:Angle = Angle::new(a.index(),b.index(),c.index());
+
+                        if  angl.value(&coordinate_vector).cos().abs() < linthre{
+                            internal_coords.push(angl);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// pub fn build_internal_coords(mol:&Molecule){
+//     internal_coords:Vec<f64> = Vec::new()
+//     for index_vec in mol.n_atoms.combinations(2){
+//         let i:usize = index_vec.0;
+//         let j:usize = index_vec.1;
+//         if mol.connectivity_matrix[[i,j]]{
+//
+//         }
+//     }
+// }
+
+pub struct Distance{
+    at_a: usize,
+    at_b: usize,
+    distance: f64,
+}
+impl Distance{
+    pub(crate) fn new(at_a:usize,at_b:usize,dist_matrix:&Array2<f64>) ->Distance {
+        let at_a:usize = at_a;
+        let at_b:usize = at_b;
+
+        let distance:f64 = dist_matrix[[at_a,at_b]];
+
+        let dist = Distance{
+            at_a: at_a,
+            at_b: at_b,
+            distance: distance,
+        };
+
+        return dist;
+    }
+}
+
+pub struct Angle{
+    at_a: usize,
+    at_b: usize,
+    at_c: usize,
+}
+
+impl Angle{
+    pub(crate) fn new(at_a:usize,at_b:usize,at_c:usize) ->Angle {
+        let at_a:usize = at_a;
+        let at_b:usize = at_b;
+        let at_c:usize = at_c;
+
+        let angle = Angle{
+            at_a: at_a,
+            at_b: at_b,
+            at_c: at_c,
+        };
+
+        return angle;
+    }
+
+    pub fn value(self,coord_vector:&Array1<f64>)->f64{
+        let a:usize = self.at_a;
+        let b:usize = self.at_b;
+        let c:usize = self.at_c;
+
+        // vector from first atom to central
+        let vec_1:Array1<f64> = coord_vector.slice(s![3*a..3*a+3]).to_owned()- coord_vector.slice(s![3*b..3*b+3]).to_owned();
+        // vector from last atom to central
+        let vec_2:Array1<f64> = coord_vector.slice(s![3*c..3*c+3]).to_owned()- coord_vector.slice(s![3*b..3*b+3]).to_owned();
+        // norm of the vectors
+        let norm_1:f64 = vec_1.norm();
+        let norm_2:f64 = vec_2.norm();
+        let dot:f64 = vec_1.dot(&vec_2);
+        let factor:f64 = dot/(norm_1*norm_2);
+
+        let mut return_value:f64 = 0.0;
+
+        if (dot/(norm_1*norm_2)) <= -1.0{
+            if ((dot/(norm_1*norm_2)).abs() +1.0) < -1e-6{
+                println!("Invalued value in angle");
+            }
+            return_value = 1.0_f64*PI;
+        }
+        else if (dot/(norm_1*norm_2)) >= 1.0{
+            if ((dot/(norm_1*norm_2)).abs() -1.0) < 1e-6{
+                println!("Invalued value in angle");
+            }
+            return_value = 0.0;
+        }
+        else {
+            return_value = (dot/(norm_1*norm_2)).acos();
+        }
+        return return_value;
+    }
+}
+
