980b73125d370b4a3fec8a05211e1de1a43ab8c6
richard <richard.einsele@gmail.com>
2021-03-02 13:11:21 +0100

    fn derivatives for out of plane

--- src/internal_coordinates.rs
+++ src/internal_coordinates.rs
@@ -975,6 +975,65 @@ impl Out_of_plane {
 
         return out_of_plane;
     }
+
+    pub fn derivatives(&self, coords: Array1<f64>) -> Array2<f64> {
+        let n_at: usize = coords.len() / 3;
+        let coords_new: Array2<f64> = coords.into_shape((n_at, 3)).unwrap();
+        let mut derivatives: Array2<f64> = Array::zeros((n_at, 3));
+        let m: usize = self.at_a;
+        let o: usize = self.at_b;
+        let p: usize = self.at_c;
+        let n: usize = self.at_d;
+
+        // unit displacement vectors
+        let u_prime: Array1<f64> =
+            coords_new.slice(s![m, ..]).to_owned() - coords_new.slice(s![o, ..]).to_owned();
+        let u_norm: f64 = u_prime.clone().to_vec().norm();
+        let v_prime: Array1<f64> =
+            coords_new.slice(s![n, ..]).to_owned() - coords_new.slice(s![p, ..]).to_owned();
+        let v_norm: f64 = v_prime.clone().to_vec().norm();
+        let w_prime: Array1<f64> =
+            coords_new.slice(s![p, ..]).to_owned() - coords_new.slice(s![o, ..]).to_owned();
+        let w_norm: f64 = w_prime.to_vec().norm();
+        let u: Array1<f64> = u_prime / u_norm;
+        let w: Array1<f64> = w_prime / w_norm;
+        let v: Array1<f64> = v_prime / v_norm;
+
+        let mut term_1: Array1<f64> = Array::zeros(3);
+        let mut term_2: Array1<f64> = Array::zeros(3);
+        let mut term_3: Array1<f64> = Array::zeros(3);
+        let mut term_4: Array1<f64> = Array::zeros(3);
+
+        if (1.0 - u.dot(&w).powi(2)) < 1e-6 {
+            term_1 = Array::from(u.to_vec().cross(&w.to_vec())) * 0.0;
+            term_3 = Array::from(u.to_vec().cross(&w.to_vec())) * 0.0;
+        } else {
+            term_1 =
+                Array::from(u.to_vec().cross(&w.to_vec())) / (u_norm * (1.0 - u.dot(&w).powi(2)));
+            term_3 = Array::from(u.to_vec().cross(&w.to_vec())) * u.dot(&w)
+                / (w_norm * (1.0 - u.dot(&w).powi(2)));
+        }
+        if (1.0 - v.dot(&w).powi(2)) < 1e-6 {
+            term_2 = Array::from(v.to_vec().cross(&w.to_vec())) * 0.0;
+            term_4 = Array::from(v.to_vec().cross(&w.to_vec())) * 0.0;
+        } else {
+            term_2 =
+                Array::from(v.to_vec().cross(&w.to_vec())) / (v_norm * (1.0 - v.dot(&w).powi(2)));
+            term_4 = Array::from(v.to_vec().cross(&w.to_vec())) * v.dot(&w)
+                / (w_norm * (1.0 - v.dot(&w).powi(2)));
+        }
+
+        derivatives.slice_mut(s![m, ..]).assign(&term_1);
+        derivatives.slice_mut(s![n, ..]).assign(&(-&term_2));
+        derivatives
+            .slice_mut(s![o, ..])
+            .assign(&(-&term_1 + &term_2 - &term_4));
+        derivatives
+            .slice_mut(s![p, ..])
+            .assign(&(&term_2 - &term_3 + &term_4));
+
+        return derivatives;
+    }
 }
 
 #[derive(Eq, PartialEq, Clone, Copy)]
