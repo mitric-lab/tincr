565032ee77c320b77a7b5eb70258532d320061a4
richard <richard.einsele@gmail.com>
2021-03-21 13:24:08 +0100

    parallelized generation of pairs

--- src/fmo.rs
+++ src/fmo.rs
@@ -128,6 +128,124 @@ pub fn fmo_calculate_pairwise(
     return (h_0_complete_mut, pair_vec);
 }
 
+pub fn fmo_calculate_pairwise_par(
+    fragments: &Vec<Molecule>,
+    cluster_results: &cluster_frag_result,
+    config: GeneralConfig,
+) -> (Array2<f64>, Vec<pair_result>) {
+    let result: Vec<pair_result> = fragments
+        .into_par_iter()
+        .enumerate()
+        .zip(fragments.into_par_iter().enumerate())
+        .filter_map(|((ind1, molecule_a), (ind2, molecule_b))| {
+            if ind1 < ind2 {
+                let mut atomic_numbers: Vec<u8> = Vec::new();
+                let mut positions: Array2<f64> =
+                    Array2::zeros((molecule_a.n_atoms + molecule_b.n_atoms, 3));
+
+                for i in 0..molecule_a.n_atoms {
+                    atomic_numbers.push(molecule_a.atomic_numbers[i]);
+                    positions
+                        .slice_mut(s![i, ..])
+                        .assign(&molecule_a.positions.slice(s![i, ..]));
+                }
+                for i in 0..molecule_b.n_atoms {
+                    atomic_numbers.push(molecule_b.atomic_numbers[i]);
+                    positions
+                        .slice_mut(s![molecule_a.n_atoms + i, ..])
+                        .assign(&molecule_b.positions.slice(s![i, ..]));
+                }
+                let pair: Molecule = Molecule::new(
+                    atomic_numbers,
+                    positions,
+                    Some(config.mol.charge),
+                    Some(config.mol.multiplicity),
+                    Some(0.0),
+                    None,
+                    config.clone(),
+                );
+                // compute Slater-Koster matrix elements for overlap (S) and 0-th order Hamiltonian (H0)
+                let (s, h0): (Array2<f64>, Array2<f64>) = h0_and_s(
+                    &pair.atomic_numbers,
+                    pair.positions.view(),
+                    pair.calculator.n_orbs,
+                    &pair.calculator.valorbs,
+                    pair.proximity_matrix.view(),
+                    &pair.calculator.skt,
+                    &pair.calculator.orbital_energies,
+                );
+                // Now select off-diagonal couplings. The block `H0_AB` contains matrix elements
+                // between atomic orbitals on fragments A and B:
+                //
+                //      ( H0_AA  H0_AB )
+                // H0 = (              )
+                //      ( H0_BA  H0_BB )
+                let mut indices_vec: Vec<(usize, usize)> = Vec::new();
+                let mut h0_vals: Vec<f64> = Vec::new();
+
+                let h0_ab: Array2<f64> = h0
+                    .slice(s![
+                        0..cluster_results.n_mo[ind1],
+                        cluster_results.n_mo[ind2]..
+                    ])
+                    .to_owned();
+                // contract Hamiltonian with coefficients of HOMOs on fragments A and B
+                let i: usize = ind1 * 2;
+                let j: usize = ind2 * 2;
+                indices_vec.push((i, j));
+                let h0_val: f64 = cluster_results.homo_orbs[ind1]
+                    .dot(&h0_ab.dot(&cluster_results.homo_orbs[ind2]));
+                h0_vals.push(h0_val);
+
+                let i: usize = ind1 * 2 + 1;
+                let j: usize = ind2 * 2 + 1;
+                indices_vec.push((i, j));
+                let h0_val: f64 =
+                    cluster_results.lumo_orbs[ind1].dot(&s.dot(&cluster_results.lumo_orbs[ind2]));
+                h0_vals.push(h0_val);
+
+                let pair_res: pair_result = pair_result::new(pair, h0_vals, indices_vec);
+
+                Some(pair_res)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let mut h_0_complete_mut: Array2<f64> = cluster_results.h_0.clone();
+    for pair in result.iter() {
+        h_0_complete_mut[[pair.h0_indices[0].0, pair.h0_indices[0].1]] = pair.h0_vals[0];
+        h_0_complete_mut[[pair.h0_indices[1].0, pair.h0_indices[1].1]] = pair.h0_vals[1];
+    }
+
+    h_0_complete_mut = h_0_complete_mut.clone()
+        + (h_0_complete_mut.clone() - Array::from_diag(&h_0_complete_mut.diag())).reversed_axes();
+
+    return (h_0_complete_mut, result);
+}
+
+pub struct pair_result {
+    pair: Molecule,
+    h0_vals: Vec<f64>,
+    h0_indices: Vec<(usize, usize)>,
+}
+
+impl pair_result {
+    pub(crate) fn new(
+        pair: Molecule,
+        h0_vals: Vec<f64>,
+        h0_indices: Vec<(usize, usize)>,
+    ) -> (pair_result) {
+        let result = pair_result {
+            pair: pair,
+            h0_vals: h0_vals,
+            h0_indices: h0_indices,
+        };
+        return result;
+    }
+}
+
 pub struct fragment_result {
     n_mo: usize,
     h_diag: Vec<f64>,
@@ -163,7 +281,7 @@ impl fragment_result {
 
 pub struct cluster_frag_result {
     n_mo: Vec<usize>,
-    h_diag: Vec<f64>,
+    h_0: Array2<f64>,
     homo_orbs: Vec<Array1<f64>>,
     lumo_orbs: Vec<Array1<f64>>,
     ind_homo: Vec<usize>,
@@ -174,7 +292,7 @@ pub struct cluster_frag_result {
 impl cluster_frag_result {
     pub(crate) fn new(
         n_mo: Vec<usize>,
-        h_diag: Vec<f64>,
+        h_0: Array2<f64>,
         homo_orbs: Vec<Array1<f64>>,
         lumo_orbs: Vec<Array1<f64>>,
         ind_homo: Vec<usize>,
@@ -183,7 +301,7 @@ impl cluster_frag_result {
     ) -> (cluster_frag_result) {
         let result = cluster_frag_result {
             n_mo: n_mo,
-            h_diag: h_diag,
+            h_0: h_0,
             homo_orbs: homo_orbs,
             lumo_orbs: lumo_orbs,
             ind_homo: ind_homo,
@@ -194,8 +312,7 @@ impl cluster_frag_result {
     }
 }
 
-pub fn fmo_calculate_fragments(fragments: &Vec<Molecule>) -> (cluster_frag_result)
-{
+pub fn fmo_calculate_fragments(fragments: &Vec<Molecule>) -> (cluster_frag_result) {
     let norb_frag: usize = 2;
     let size: usize = norb_frag * fragments.len();
 
@@ -253,9 +370,10 @@ pub fn fmo_calculate_fragments(fragments: &Vec<Molecule>) -> (cluster_frag_resul
         ind_lumo.push(frag_res.ind_lumo);
         energies.push(frag_res.energy);
     }
+    let h_0: Array2<f64> = Array::from_diag(&Array::from(h_diag));
 
     let cluster_result: cluster_frag_result = cluster_frag_result::new(
-        n_mo, h_diag, homo_orbs, lumo_orbs, ind_homo, ind_lumo, energies,
+        n_mo, h_0, homo_orbs, lumo_orbs, ind_homo, ind_lumo, energies,
     );
 
     //let h_0: Array2<f64> = Array::from_diag(&Array::from(h_diag));
