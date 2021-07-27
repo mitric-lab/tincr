3992f739a9c6558d17f99543b5baca72564f07db
einseler <richard.einsele@gmail.com>
2021-07-27 14:04:13 +0200

    excited gradient routines for lc and no_lc

--- src/gradients/excited_state.rs
+++ src/gradients/excited_state.rs
@@ -1,4 +1,4 @@
-use crate::gradients::helpers::{f_lr, f_v, h_minus, zvector_lc, Hplus, HplusType};
+use crate::gradients::helpers::{f_lr, f_v, h_minus, zvector_lc, Hplus, HplusType, h_plus_no_lr, zvector_no_lc};
 use crate::initialization::*;
 use crate::utils::ToOwnedF;
 use ndarray::{s, Array, Array1, Array2, Array3, Array4, ArrayView1, ArrayView2, ArrayView3, Axis};
@@ -6,7 +6,7 @@ use ndarray_linalg::{into_col, into_row, IntoTriangular, Solve, UPLO};
 use std::time::Instant;
 
 impl System {
-    pub fn excited_state_gradient(&mut self, state: usize) -> Array1<f64> {
+    pub fn excited_state_gradient_lc(&mut self, state: usize) -> Array1<f64> {
         // set the occupied and virtual orbital energies
         let orbe: ArrayView1<f64> = self.properties.orbe().unwrap();
         let orbe_occ: Array1<f64> = self.occ_indices.iter().map(|&occ| orbe[occ]).collect();
@@ -283,6 +283,214 @@ impl System {
                     .into_shape([3 * self.n_atoms, self.n_orbs * self.n_orbs])
                     .unwrap()
                     .dot(&xmy_ao.view().into_shape(self.n_orbs * self.n_orbs).unwrap());
+
+        return gradExc;
+    }
+
+    pub fn excited_state_gradient_no_lc(&mut self, state: usize) -> Array1<f64> {
+        // set the occupied and virtual orbital energies
+        let orbe: ArrayView1<f64> = self.properties.orbe().unwrap();
+        let orbe_occ: Array1<f64> = self.occ_indices.iter().map(|&occ| orbe[occ]).collect();
+        let orbe_virt: Array1<f64> = self.virt_indices.iter().map(|&virt| orbe[virt]).collect();
+
+        // transform the energies to a diagonal 2d matrix
+        let ei: Array2<f64> = Array2::from_diag(&orbe_occ);
+        let ea: Array2<f64> = Array2::from_diag(&orbe_virt);
+
+        let n_occ: usize = orbe_occ.len();
+        let n_virt: usize = orbe_virt.len();
+
+        // take state specific values from the excitation vectors
+        let xmy_state: Array2<f64> = self
+            .properties
+            .take_xmy()
+            .unwrap()
+            .slice(s![state, .., ..])
+            .to_owned();
+        let xpy_state: Array2<f64> = self
+            .properties
+            .take_xpy()
+            .unwrap()
+            .slice(s![state, .., ..])
+            .to_owned();
+        // excitation energy of the state
+        let omega_state: f64 = self.properties.excited_states().unwrap()[state];
+
+        // calculate the vectors u, v and t
+        let u_ab: Array2<f64> = xpy_state.t().dot(&xmy_state) + xmy_state.t().dot(&xpy_state);
+        let u_ij: Array2<f64> = xpy_state.dot(&xmy_state.t()) + xmy_state.dot(&xpy_state.t());
+
+        let v_ab: Array2<f64> =
+            ei.dot(&xpy_state).t().dot(&xpy_state) + ei.dot(&xmy_state).t().dot(&xmy_state);
+        let v_ij: Array2<f64> =
+            xpy_state.dot(&ea).dot(&xpy_state.t()) + xmy_state.dot(&ea).dot(&xmy_state.t());
+
+        let t_ab: Array2<f64> =
+            0.5 * (xpy_state.t().dot(&xpy_state) + xmy_state.t().dot(&xmy_state));
+        let t_ij: Array2<f64> =
+            0.5 * (xpy_state.dot(&xpy_state.t()) + xmy_state.dot(&xmy_state.t()));
+
+        // get the transition charges
+        let qtrans_ov: Array3<f64> = self.properties.take_qtrans_ov().unwrap();
+        let qtrans_oo: Array3<f64> = self.properties.take_qtrans_oo().unwrap();
+        let qtrans_vv: Array3<f64> = self.properties.take_qtrans_vv().unwrap();
+
+        // set gamma matrix
+        let g0: ArrayView2<f64> = self.properties.gamma().unwrap();
+
+        // compute hplus of tab and tij
+        let hplus_tab: Array2<f64> = h_plus_no_lr(g0,qtrans_oo.view(),qtrans_vv.view(),t_ab.view());
+        let hplus_tij: Array2<f64> = h_plus_no_lr(g0,qtrans_oo.view(),qtrans_oo.view(),t_ij.view());
+
+        // calculate q_ij
+        let g_ij: Array2<f64> = hplus_tab - hplus_tij;
+        let q_ij: Array2<f64> = omega_state * u_ij - v_ij + g_ij;
+
+        // calculate q_ab
+        let q_ab: Array2<f64> = omega_state * u_ab + v_ab;
+
+        // calculate q_ia
+        let mut q_ia: Array2<f64> = xpy_state.dot(
+            &h_plus_no_lr(g0,qtrans_vv.view(),qtrans_ov.view(),xpy_state.view()).t());
+        q_ia = q_ia + h_plus_no_lr(g0,qtrans_ov.view(),qtrans_vv.view(),t_ab.view());
+        q_ia = q_ia - h_plus_no_lr(g0,qtrans_ov.view(),qtrans_oo.view(),t_ij.view());
+
+        // calculate q_ai
+        let q_ai: Array2<f64> =
+            xpy_state
+                .t()
+                .dot(&h_plus_no_lr(g0,qtrans_oo.view(),qtrans_ov.view(),xpy_state.view()));
+
+        // calculate right hand side of the z-vector equation
+        let r_ia: Array2<f64> = &q_ai.t() - &q_ia;
+
+        // input for zvector routine
+        let omega_input: Array2<f64> = into_col(Array::ones(orbe_occ.len()))
+            .dot(&into_row(orbe_virt.clone()))
+            - into_col(orbe_occ.clone()).dot(&into_row(Array::ones(orbe_virt.len())));
+        let r_ia_flat: Array1<f64> = r_ia.t().to_owned_f().into_shape((n_occ * n_virt)).unwrap();
+        let r_matrix: Array2<f64> = r_ia_flat.into_shape((n_occ, n_virt)).unwrap();
+
+        // calculate the z-vector
+        let z_ia: Array2<f64> = zvector_no_lc(
+            omega_input.view(),
+            r_matrix.view(),
+            g0,
+            qtrans_oo.view(),
+            self.config.mol.multiplicity,
+            Array1::zeros(self.n_atoms).view(),
+        );
+
+        // calculate w_ij
+        let mut w_ij: Array2<f64> = q_ij + h_plus_no_lr(g0,qtrans_oo.view(),qtrans_ov.view(),z_ia.view());
+        for i in 0..w_ij.dim().0 {
+            w_ij[[i, i]] = w_ij[[i, i]] / 2.0;
+        }
+        // w_ia
+        let w_ia: Array2<f64> = &q_ai.t() + &ei.dot(&z_ia);
+
+        // w_ai
+        let w_ai: Array2<f64> = w_ia.clone().reversed_axes();
+
+        // w_ab
+        let mut w_ab: Array2<f64> = q_ab;
+        for i in 0..w_ab.dim().0 {
+            w_ab[[i, i]] = w_ab[[i, i]] / 2.0;
+        }
+
+        // build w matrix: combine w_ij, w_ia, w_ai and w_ab
+        let length: usize = n_occ + n_virt;
+        let mut w_matrix: Array2<f64> = Array::zeros((length, length));
+        for i in 0..w_ij.dim().0 {
+            w_matrix
+                .slice_mut(s![i, ..w_ij.dim().1])
+                .assign(&w_ij.slice(s![i, ..]));
+            w_matrix
+                .slice_mut(s![i, w_ij.dim().1..])
+                .assign(&w_ia.slice(s![i, ..]));
+        }
+        for i in 0..w_ai.dim().0 {
+            w_matrix
+                .slice_mut(s![w_ij.dim().0 + i, ..w_ai.dim().1])
+                .assign(&w_ai.slice(s![i, ..]));
+            w_matrix
+                .slice_mut(s![w_ij.dim().0 + i, w_ai.dim().1..])
+                .assign(&w_ab.slice(s![i, ..]));
+        }
+
+        // get arrays from properties
+        let diff_p: Array2<f64> = &self.properties.p().unwrap() - &self.properties.p_ref().unwrap();
+        let grad_s: Array3<f64> = self.properties.take_grad_s().unwrap();
+        let g0_ao: Array2<f64> = self.properties.take_gamma_ao().unwrap();
+        let g1_ao: Array3<f64> = self.properties.take_grad_gamma_ao().unwrap();
+        let grad_h:Array3<f64> = self.properties.take_grad_h0().unwrap();
+        let s: ArrayView2<f64> = self.properties.s().unwrap();
+
+        // calculate gradH: gradH0 + gradHexc
+        let f_dmd0: Array3<f64> = f_v(
+            diff_p.view(),
+            s,
+            grad_s.view(),
+            g0_ao.view(),
+            g1_ao.view(),
+            self.n_atoms,
+            self.n_orbs,
+        );
+        let grad_h: Array3<f64> = grad_h + f_dmd0;
+
+        // set the occupied and virtuals orbital coefficients
+        let orbs: ArrayView2<f64> = self.properties.orbs().unwrap();
+        let mut orbs_occ: Array2<f64> = Array::zeros((self.n_orbs, n_occ));
+        let mut orbs_virt: Array2<f64> = Array::zeros((self.n_orbs, n_virt));
+        for (i, index) in self.occ_indices.iter().enumerate() {
+            orbs_occ.slice_mut(s![.., i]).assign(&orbs.column(*index));
+        }
+        for (i, index) in self.virt_indices.iter().enumerate() {
+            orbs_virt.slice_mut(s![.., i]).assign(&orbs.column(*index));
+        }
+
+        // transform t and z vectors to AO basis
+        let t_oo: Array2<f64> = orbs_occ.dot(&t_ij.dot(&orbs_occ.t()));
+        let t_vv: Array2<f64> = orbs_virt.dot(&t_ab.dot(&orbs_virt.t()));
+        let z_ao: Array2<f64> = orbs_occ.dot(&z_ia.dot(&orbs_virt.t()));
+
+        // transform w matrix and excited state vectors to AO basis
+        let w_triangular: Array2<f64> = w_matrix.into_triangular(UPLO::Upper);
+        let w_ao: Array2<f64> = orbs.dot(&w_triangular.dot(&orbs.t()));
+        let xpy_ao:Array2<f64> = orbs_occ.dot(&xpy_state.dot(&orbs_virt.t()));
+        let xmy_ao:Array2<f64> = orbs_occ.dot(&xmy_state.dot(&orbs_virt.t()));
+
+        // calculate contributions to the excited gradient
+        let f: Array3<f64> = f_v(
+            xpy_ao.view(),
+            s,
+            grad_s.view(),
+            g0_ao.view(),
+            g1_ao.view(),
+            self.n_atoms,
+            self.n_orbs,
+        );
+
+        // assemble the excited gradient
+        let mut gradExc: Array1<f64> = Array::zeros(3 * self.n_atoms);
+        // gradH * (T + Z)
+        gradExc = gradExc
+            + grad_h
+            .into_shape([3 * self.n_atoms, self.n_orbs * self.n_orbs])
+            .unwrap()
+            .dot(&(t_vv - t_oo + z_ao).into_shape(self.n_orbs * self.n_orbs).unwrap());
+        // - gradS * W
+        gradExc = gradExc
+            - grad_s
+            .into_shape([3 * self.n_atoms, self.n_orbs * self.n_orbs])
+            .unwrap()
+            .dot(&w_ao.into_shape(self.n_orbs * self.n_orbs).unwrap());
+        // 2.0 * sum (X+Y) F (X+Y)
+        gradExc = gradExc
+            + 2.0
+            * f.into_shape([3 * self.n_atoms, self.n_orbs * self.n_orbs])
+            .unwrap()
+            .dot(&xpy_ao.view().into_shape(self.n_orbs * self.n_orbs).unwrap());
 
         return gradExc;
     }
